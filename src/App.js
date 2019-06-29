52ee3e8897b27f81c17f14703a3a0836f8cf5173
Marzena <marzena.szopinska090@gmail.com>
2019-06-29 08:43:47 +0200

    set up the main components

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
+import React from 'react';
 import './App.css';
+
+class App extends React.Component {
+
+    render(){
+        return (
+            <div className='container'>
+                <h1>Hello World!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
+// Render the whole app
+ReactDOM.render(<App />, document.getElementById('root'));
